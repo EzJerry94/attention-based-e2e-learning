795e625bc6bdf22b47d4c5e109866bcd5a4a093b
武秉文 <bingwenwu@foxmail.com>
2018-07-16 11:51:48 +0100

    read the duration of each single wav file

--- multi/tfrecord_generator.py
+++ multi/tfrecord_generator.py
@@ -1,15 +1,27 @@
 import numpy as np
+from moviepy.editor import AudioFileClip
+
+
 class Generator():
 
     def __init__(self, csv):
         self.attributes_name = ['file', 'arousal', 'valence', 'dominance']
-        self.attributes_typt = ['str', 'int', 'int']
+        self.attributes_type = ['str', 'int', 'int', 'int']
         self.csv = csv
 
     def read_csv(self, file):
-        lines = np.loadtxt(file, dtype='str')
-        print(lines)
+        self.data = np.loadtxt(file, dtype='str')
+        print(self.data.shape)
+
+    def read_single_data(self, file):
+        clip = AudioFileClip
+        end_time = clip(file).duration
         pass
 
     def write_tfrecords(self):
         self.read_csv(self.csv)
+        self.dict_files = dict()
+        for row in self.data:
+            print(row)
+            time = self.read_single_data(row[0])
+        pass
