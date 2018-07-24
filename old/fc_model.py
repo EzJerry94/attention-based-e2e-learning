6a473b8088ba6120509d75d7209f9ebde2f4aee2
武秉文 <bingwenwu@foxmail.com>
2018-07-24 10:11:41 +0100

    start over !!!

--- old/fc_model.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-slim = tf.contrib.slim
-
-def fully_connected(inputs:tf.Tensor,
-                    number_of_outputs:int):
-    with tf.variable_scope("fully_connected", reuse=tf.AUTO_REUSE):
-        fc = slim.layers.linear(inputs, number_of_outputs)
-    return fc
